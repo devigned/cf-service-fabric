9c3dcea338bc6401a6e702ada93482189fdbc715
David Justice <david@devigned.com>
2016-11-11 10:26:52 -0800

    tune up the scripts a bit

--- tools/update-sf-cluster.py
+++ tools/update-sf-cluster.py
@@ -2,22 +2,15 @@
 
 import boto3
 import sys
-import json
-from subprocess import Popen, PIPE, STDOUT
+import jinja2
 
 
-def run_cmd(cmd):
-    print "Running: %s" % cmd
-    p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
-    output = p.stdout.read()
-    output.strip()
-    return output
-
-
-if len(sys.argv) < 2:
-    print "usage: python update-sf-cluster.py stack-id"
+if len(sys.argv) < 3:
+    print "usage: python render-manifest.py stack-id file-source"
     exit(1)
 stack_id = sys.argv[1]
+file_source = sys.argv[2]
+
 
 client = boto3.client('ec2')
 
@@ -30,4 +23,8 @@ for reservation in reservations:
         for interface in instance['NetworkInterfaces']:
             private_ips.add(interface['PrivateIpAddress'])
 
-print json.dumps({'ips': list(private_ips)})
+context = {'ips': list(private_ips)}
+
+with open(file_source, 'r') as f:
+    print jinja2.Template(f.read()).render(context)
+
